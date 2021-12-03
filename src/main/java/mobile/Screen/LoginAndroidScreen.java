4c0b6fffb804729d07417a7ca22c158dac37f49b
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-12-03 08:32:13 +0530

    Ios test for the application

---
+++ src/main/java/mobile/Screen/LoginAndroidScreen.java
@@ -0,0 +1,26 @@
+package mobile.Screen;
+
+import com.testvagrant.ekam.atoms.mobile.MobileScreen;
+import com.testvagrant.ekam.reports.annotations.MobileStep;
+import org.openqa.selenium.By;
+
+public class LoginAndroidScreen extends MobileScreen {
+    private final By usernameTextBox = queryByContentDesc("test-Username");
+    private final By passwordTextBox = queryByContentDesc("test-Password");
+    private final By loginButton = queryByContentDesc("test-LOGIN");
+    private final By cart = queryByContentDesc("test-Cart");
+
+    @MobileStep(keyword = "When", description = "I login")
+    public LoginAndroidScreen login() {
+        textbox(usernameTextBox).setText("standard_user");
+        textbox(passwordTextBox).setText("secret_sauce");
+        element(loginButton).click();
+        return this;
+    }
+
+    @MobileStep(keyword = "Then", description = "If cart is displayed")
+    public boolean isCartDisplayed() {
+        return element(cart).isDisplayed();
+    }
+
+}
